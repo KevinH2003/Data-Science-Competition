a382de104f71cece2b0b5ecb0f8eaa66850a9633
Kevin Huang <klh124@duke.edu>
2024-04-07 21:45:12 -0400

    writing results and logging

--- comprehensive_testing.py
+++ comprehensive_testing.py
@@ -8,21 +8,31 @@ from sklearn.linear_model import Lasso
 from warnings import simplefilter
 from dgp import DataGenerator
 from variable_importance_scoring import importance_score, cross_validation_scores
+import os
 
+RESULTS_FOLDER = "results_folder"
+
+# Create the folder if it doesn't exist
+if not os.path.exists(RESULTS_FOLDER):
+    os.makedirs(RESULTS_FOLDER)
+
+print("Starting...")
+
+###Parameter Grids###
 param_grid_lasso = {
     'alpha': [1e-2, 1e-1, 1, 10],  # A range from very small to large alpha values
     'max_iter': [1000, 5000, 10000],  # Maximum number of iterations to converge
     'tol': [1e-4, 1e-3, 1e-2]  # Tolerance for the optimization
 }
 
-'''
+
 param_grid_xgb = {
     'learning_rate': [0.01, 0.05, 0.1],  # Smaller values make the model more robust.
     'n_estimators': [100, 300, 500],  # More trees can be better, but at the risk of overfitting.
     'max_depth': [3, 5, 7]
 }
-'''
 
+'''
 param_grid_xgb = {
     'learning_rate': [0.01, 0.05, 0.1],  # Smaller values make the model more robust.
     'n_estimators': [100, 500, 1000],  # More trees can be better, but at the risk of overfitting.
@@ -35,17 +45,19 @@ param_grid_xgb = {
     'reg_alpha': [0, 0.1, 0.5],  # L1 regularization term, larger values specify stronger regularization.
     'scale_pos_weight': [1]  # Use only if you have a highly imbalanced class distribution.
 }
+'''
+n_iter = 5
 
-n_iter = 10
+dgps = {
+    "Toy_With_Noise": DataGenerator(num_cols=100, num_rows=100, num_important=10, num_interaction_terms=0, effects='constant', noise=1),
+    #"1000_Cols_Highly_Correlated": DataGenerator(num_cols=1000, num_rows=100, num_important=10, num_interaction_terms=50, effects='linear', correlation_range=[-0.9, 0.9], noise=5),
+    #"Corn_Mimic": DataGenerator(num_cols=50000, num_rows=160, num_important=10, num_interaction_terms=500, effects='all', correlation_range=[-0.95, 0.95]),
+    #"100_Cols_50_Interaction_Terms": DataGenerator(num_cols=100, num_rows=1000, num_important=10, num_interaction_terms=50, effects='all', correlation_range=[-1, 1])
+}
 
-dgp_names = ["Toy Dataset w/ Noise", "1000 Cols, Highly Correlated", "Corn Mimic", "100 Cols and 50 Interaction Terms"]
-dgps = [
-    DataGenerator(num_cols=100, num_rows=100, num_important=10, num_interaction_terms=0, effects='constant', noise=1),
-    DataGenerator(num_cols=1000, num_rows=100, num_important=10, num_interaction_terms=50, effects='linear', correlation_range=[-0.9, 0.9], noise=5),
-    DataGenerator(num_cols=50000, num_rows=160, num_important=10, num_interaction_terms=500, effects='all', correlation_range=[-0.95, 0.95]),
-    DataGenerator(num_cols=100, num_rows=1000, num_important=10, num_interaction_terms=50, effects='all', correlation_range=[-1, 1])
-]
+datasets = {name: dgp.generate_data() for name, dgp in dgps.items()}
 
+print("Datasets Generated...")
 model_names = ["LASSO", "XGBoost"]
 models = [Lasso(), XGBRegressor()]
 param_grids= [param_grid_lasso, param_grid_xgb]
@@ -53,12 +65,12 @@ importance_attrs = ['coef_', 'feature_importances_']
 
 simplefilter(action="ignore", category=pd.errors.PerformanceWarning)
 warnings.filterwarnings("ignore")
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 aggregated_scores = {}
 
-for i in range(len(dgps)):
-    dgp = dgps[i]
-    dataset = dgp.generate_data()
+for name, dataset in datasets.items():
+    print(f"{name}:")
     X = dataset.drop(["target"], axis=1)
     y = dataset["target"]
 
@@ -69,7 +81,17 @@ for i in range(len(dgps)):
         param_grid = param_grids[i]
         importance_attr = importance_attrs[i]
 
-        rscv = GridSearchCV(model, param_grid, scoring='r2', verbose=0)
-        model_scores[model_names[i]] = (cross_validation_scores(rscv, X, y, importance_attr=importance_attr, true_importances=dgp.importances, verbose=True))
+        rscv = RandomizedSearchCV(model, param_grid, scoring='r2', verbose=0, n_iter=n_iter, n_jobs=-1)
+        model_scores[model_names[i]] = (cross_validation_scores(rscv, X, y, importance_attr=importance_attr, true_importances=dgps[name].importances, verbose=True))
 
-    aggregated_scores[dgp_names[i]] = model_scores
+    aggregated_scores[name] = pd.DataFrame(model_scores)
+
+for name in datasets.keys():
+    filename = name + "_results.csv"
+    file_path = os.path.join(RESULTS_FOLDER, filename)
+    
+    aggregated_scores[name].to_csv(file_path, index=True)
+
+    print(f"Results saved to {filename}")
+
+print("All Done!")
