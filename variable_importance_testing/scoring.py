b6689fadebb6e4cc6bfc9cc8edf01ee48fc6d4b1
Kevin Huang <klh124@duke.edu>
2024-05-24 11:41:16 -0400

    fix ranking confusion, update documentation

--- variable_importance_testing/scoring.py
+++ variable_importance_testing/scoring.py
@@ -1,6 +1,7 @@
 from collections import deque
 from datetime import datetime
 import os
+import numpy as np
 import pandas as pd
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV, train_test_split
 from sklearn.metrics import r2_score
@@ -12,17 +13,17 @@ import warnings
 
 from variable_importance_testing.pipelining import VI_Pipeline, FeatureSelector
 
-def rank_importances(importances):
+def importance_ranks(importances):
     """
-    Rank variables by importance
+    Rank variables by importance and return a list of relative rankings
 
     Parameters:
     importances (list-like): Variable importances, with the index 
         corresponding to the variable and importances[x] corresponding to x's importance
 
     Returns:
-    list: The ranking of variables by importance, with index being the rank
-        (ranks[i] = x means x is the i+1st most important variable)
+    list: A list of variables and their ranking, with index being the variable and 
+        the value being its relative ranking (ranks[i] = x means i is the xth most important variable)
     """
     linked = [(i, importances[i]) for i in range(len(importances))]
     linked = sorted(linked, key=lambda x: -x[1])
@@ -32,36 +33,69 @@ def rank_importances(importances):
 
     return ranks
 
-###TODO: Implement top n scoring (x/n important variables are in top n => x/n score)
+def rank_variables(importances):
+    return sorted(range(len(importances)), key=lambda i: importances[i], reverse=True)
+
+def top_n_score(true_importances, pred_importances, n=None):
+    """
+    Given n important variables in the ground truth (or the top n variables), 
+    score how many important variables appear in the top n of the
+    predicted importances. 
+
+    Parameters:
+    - true_importances (list-like): The ground truth importances.
+    - pred_importances (list-like): The predicted importances.
+    - n (int, optional): How many variables to consider at the top of the rankings.
+        (defaults to the number of variables with nonzero importance in the ground truth)
+
+    Returns:
+    - float: The importance score, defined as (correct_vars / n) where correct_vars
+        is the number of variables shared between the top n of the ground truth 
+        and the top n of the predicted importances.
+
+    """
+    if n is None:
+        n = sum([1 if x !=0 else 0 for x in true_importances])
+    
+    ranked_true = rank_variables(true_importances)[:n]
+    ranked_pred = rank_variables(pred_importances)[:n]
+
+    correct_vars = 0
+
+    for variable in ranked_pred:
+        if variable in ranked_true:
+            correct_vars += 1
+
+    return correct_vars / n
+
 def importance_score(pred_importances, true_importances, 
-                     score=spearmanr, scramble=True, num_scrambles=5, ranked=False):
+                     score=top_n_score, scramble=False, num_scrambles=5, ranked=False):
     """
     Calculate the efficacy of a variable importance prediction against a ground truth.
 
     Parameters:
-    pred_importances (array-like): The predicted importances.
-    true_importances (array-like): The ground truth importances.
-    score (function, optional): A function to compute the correlation (default is spearmanr).
-    scramble (bool, optional): Whether to scramble non-important variables in the true importances. 
-        This isn't necessary for non-ranking based scoring functions such as pearson correlation.  
-        (default is True)
-    num_scrambles (int, optional): The number of scrambles to perform (default is 5).
-    ranked (bool, optional): Whether to return a separate ranking of the variables by importance 
+    - pred_importances (list-like): The predicted importances.
+    - true_importances (list-like): The ground truth importances.
+    - score (function, optional): A function to compute the correlation (default is spearmanr).
+    - scramble (bool, optional): Whether to scramble non-important variables in the true importances. 
+        This primarily applies when using spearmanr. (default is False)
+    - num_scrambles (int, optional): The number of scrambles to perform (default is 5).
+    - ranked (bool, optional): Whether to return a separate ranking of the variables by importance 
         If True, will return a tuple of (importance score, ranks). (default is False).
 
     Returns:
-    float: The importance score.
-    list (optional): The ranks of the predicted importances (if ranked is True).
+    - float: The importance score.
+    - list (optional): The ranks of the predicted importances (if ranked is True).
     """
     
     # Copy and scale inputs
     scaler = MinMaxScaler()
     
-    pred_importances = pred_importances.copy()
-    true_importances = true_importances.copy()
+    pred_importances = np.array(pred_importances).reshape(-1, 1)
+    true_importances = np.array(true_importances).reshape(-1, 1)
 
-    pred_importances = scaler.fit_transform(pred_importances)
-    true_importances = scaler.fit_transform(true_importances)
+    pred_importances = scaler.fit_transform(pred_importances).flatten()
+    true_importances = scaler.fit_transform(true_importances).flatten()
 
     #Scramble non-important variables in true importances and get score
     if scramble:
@@ -84,13 +118,16 @@ def importance_score(pred_importances, true_importances,
     
     # Calculate the correlation between true and predicted importances
     try:
-        correlation, _ = score(true_importances, pred_importances)
+        correlation = score(true_importances, pred_importances)
+        if isinstance(correlation, tuple):
+            correlation = correlation[0]
+            
         if scramble:
             correlation = correlation / scrambled_max
 
         if ranked:
             # Rank the predicted importances
-            ranks = rank_importances(pred_importances=pred_importances)
+            ranks = importance_ranks(pred_importances)
 
     except Exception as e:
         print(e)
@@ -102,25 +139,25 @@ def importance_score(pred_importances, true_importances,
             return correlation, ranks
         return correlation
 
-def model_importance_score(model, true_importances, importance_attr=None, score=spearmanr, 
-                           absolute_value=True, scramble=True, num_scrambles=5, ranked=False):
+def model_importance_score(model, true_importances, importance_attr=None, score=top_n_score, 
+                           absolute_value=True, scramble=False, num_scrambles=5, ranked=False):
     """
     Calculate the importance score of a model's variable importance predictions against the ground truth.
 
     Parameters:
-    model (object): The model object containing variable importance attributes.
-    true_importances (array-like): The ground truth importances.
-    importance_attr (str, optional): The attribute name for the model's predicted importances. If None, attempts to infer.
-    score (function, optional): A function to compute the correlation (default is spearmanr).
-    absolute_value (bool, optional): Whether to take the absolute value of the predicted importances (default is True).
-    scramble (bool, optional): Whether to scramble non-important variables in the true importances
+    - model (object): The model object containing variable importance attributes.
+    - true_importances (array-like): The ground truth importances.
+    - importance_attr (str, optional): The attribute name for the model's predicted importances. If None, attempts to infer.
+    - score (function, optional): A function to compute the correlation (default is spearmanr).
+    - absolute_value (bool, optional): Whether to take the absolute value of the predicted importances (default is True).
+    - scramble (bool, optional): Whether to scramble non-important variables in the true importances
         (see importance_score scramble parameter) (default is True).
-    num_scrambles (int, optional): The number of scrambles to perform (default is 5).
-    ranked (bool, optional): Whether to return the ranked importances (default is False).
+    - num_scrambles (int, optional): The number of scrambles to perform (default is 5).
+    - ranked (bool, optional): Whether to return the ranked importances (default is False).
 
     Returns:
-    float: The importance score.
-    list (optional): The ranks of the predicted importances (if ranked is True).
+    - float: The importance score.
+    - list (optional): The ranks of the predicted importances (if ranked is True).
     """
     
     # Attempt to infer importance attribute if None provided
@@ -156,27 +193,27 @@ def importance_scores(model,
     evaluate one or more metrics of variable importance on one or more ground truths.
 
     Parameters:
-    model (object): The model object to evaluate. 
+    - model (object): The model object to evaluate. 
         (Or a CV object such as GridSearchCV or RandomizedSearchCV if cross-validate is True)
-    X (array-like): The feature dataset.
-    y (array-like): The target variable.
-    true_importances (list or dict): The ground truth importances.
-    test_size (float, optional): The proportion of the dataset to include in the test split (default is 0.3).
-    importance_attr (str, optional): The attribute name for the model's predicted importances. 
+    - X (array-like): The feature dataset.
+    - y (array-like): The target variable.
+    - true_importances (list or dict): The ground truth importances.
+    - test_size (float, optional): The proportion of the dataset to include in the test split (default is 0.3).
+    - importance_attr (str, optional): The attribute name for the model's predicted importances. 
         If None, will attempt to infer (see model_importance_score)
-    score_functions (dict or callable, optional): A dictionary of scoring functions 
+    - score_functions (dict or callable, optional): A dictionary of scoring functions 
         or a single scoring function (default is {"model importance": model_importance_score}).
-    cross_validate (bool, optional): Whether to perform cross-validation. 
+    - cross_validate (bool, optional): Whether to perform cross-validation. 
         (If True, model must be a CV object) (default is False)
-    include_results (bool, optional): Whether to include cross-validation results. 
+    - include_results (bool, optional): Whether to include cross-validation results. 
         For disambiguation, this refers solely to the .cv_results_ parameter of the CV object.
         The best model and parameters from the CV will always be returned if cross-validate is True.
         (cross-validate must be True for this to have any effect) (default is False).
-    ranked (bool, optional): Whether to return the ranked importances (default is False).
-    verbose (bool, optional): Whether to print verbose output (default is False).
+    - ranked (bool, optional): Whether to return the ranked importances (default is False).
+    - verbose (bool, optional): Whether to print verbose output (default is False).
 
     Returns:
-    dict: A dictionary containing the importance scores, R^2 scores, completion times, 
+    - dict: A dictionary containing the importance scores, R^2 scores, completion times, 
         and optional cross-validation results and importance rankings.
     """
     
@@ -266,8 +303,8 @@ def importance_scores(model,
             #note: maybe change this so it can accept all true importances at once to save time
             if ranked:
                 scores[importance + "_" + score_func + "_score"], scores["ranks"][importance][score_func] = score_functions[score_func](model=best_model, X=X_train, y=y_train, true_importances=true_importances[importance], importance_attr=importance_attr, ranked=ranked)
-                print(importance, score_func)
-                print(scores[importance + "_" + score_func + "_score"], scores["ranks"][importance][score_func])
+                #print(importance, score_func)
+                #print(scores[importance + "_" + score_func + "_score"], scores["ranks"][importance][score_func])
             else:
                 scores[importance + "_" + score_func + "importance_score"] = score_functions[score_func](model=best_model, X=X_train, y=y_train, true_importances=true_importances[importance], importance_attr=importance_attr, ranked=ranked)
         
@@ -311,33 +348,32 @@ def importance_testing(models,
                        verbose=True
                        ):
     """
-    A comprehensive testing loop over multiple models, datasets, and variable importance metrics,
-
+    A comprehensive testing loop over multiple models, datasets, and variable importance metrics
 
     Parameters:
-    models (dict-like): Dictionary of model names and their corresponding model objects.
-    param_grids (dict-like): Dictionary of model names and their 
+    - models (dict-like): Dictionary of model names and their corresponding model objects.
+    - param_grids (dict-like): Dictionary of model names and their 
         corresponding parameter grids for hyperparameter tuning.
-    datasets (dict-like): Dictionary of dataset names and their corresponding dataframes.
-    true_importances (dict-like): Dictionary of dataset names and their corresponding true importances.
-    score_functions (dict-like or callable, optional): Dictionary of scoring 
+    - datasets (dict-like): Dictionary of dataset names and their corresponding dataframes.
+    - true_importances (dict-like): Dictionary of dataset names and their corresponding true importances.
+    - score_functions (dict-like or callable, optional): Dictionary of scoring 
         functions or a single scoring function (default is {'model_importance': model_importance_score}).
-    importance_attrs (dict-like, optional): Dictionary of model names 
+    - importance_attrs (dict-like, optional): Dictionary of model names 
         and their corresponding importance attribute names.
-    trimming_steps (dict-like, optional): Dictionary of model names and their corresponding trimming steps.
-    final_predictors (dict-like, optional): Dictionary of final predictor names 
+    - trimming_steps (dict-like, optional): Dictionary of model names and their corresponding trimming steps.
+    - final_predictors (dict-like, optional): Dictionary of final predictor names 
         and their corresponding model objects.
-    n_iters (dict-like or int, optional): Number of iterations for randomized search. 
+    - n_iters (dict-like or int, optional): Number of iterations for randomized search. 
         If an int, the same value is used for all models.
-    num_folds (int, optional): Number of folds for cross-validation (default is 3).
-    ranked (bool, optional): Whether to return ranked importances (default is False).
-    grid_search (bool, optional): Whether to use GridSearchCV instead of RandomizedSearchCV (default is False).
-    save_results (bool, optional): Whether to save the results to CSV files (default is True).
-    results_folder (str, optional): Folder to save the results (default is "importance_testing_results").
-    verbose (bool, optional): Whether to print verbose output (default is True).
+    - num_folds (int, optional): Number of folds for cross-validation (default is 3).
+    - ranked (bool, optional): Whether to return ranked importances (default is False).
+    - grid_search (bool, optional): Whether to use GridSearchCV instead of RandomizedSearchCV (default is False).
+    - save_results (bool, optional): Whether to save the results to CSV files (default is True).
+    - results_folder (str, optional): Folder to save the results (default is "importance_testing_results").
+    - verbose (bool, optional): Whether to print verbose output (default is True).
 
     Returns:
-    dict: Aggregated scores for each dataset.
+    - dict: Aggregated scores for each dataset.
     """
      
     if verbose:
